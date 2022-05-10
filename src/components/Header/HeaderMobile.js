f246571ca8fc0db43bc5321b6b3fc7d56a74951f
Martin Rombach <martinrombach88@gmail.com>
2022-05-10 15:16:04 +0900

    header mobile

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -30,7 +30,7 @@ const HeaderMobile = ({ headerStyle }) => {
       width="20"
       height="20"
       fill="currentColor"
-      class="bi bi-x-lg"
+      className="bi bi-x-lg"
       viewBox="0 0 16 16"
     >
       <path d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
