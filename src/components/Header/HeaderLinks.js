e57c97a9c3985fc9f8fc2a93d6fda353a0091942
Martin Rombach <martinrombach88@gmail.com>
2022-05-10 15:09:06 +0900

    header mobile

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 
-const HeaderLinks = () => {
+const HeaderLinks = ({ mobile }) => {
+  let linkClass = mobile ? "header__MobBtn" : "header__Btn";
   return (
     <>
-      <Link to="/media" className="header__Btn">
+      <Link to="/media" className={linkClass}>
         WORK
       </Link>
-      <Link to="/news" className="header__Btn">
+      <Link to="/news" className={linkClass}>
         NEWS
       </Link>
-      <Link to="/about" className=" header__Btn">
+      <Link to="/about" className={linkClass}>
         ABOUT
       </Link>
-      <Link to="/history" className=" header__Btn">
+      <Link to="/history" className={linkClass}>
         HISTORY
       </Link>
-      <Link to="/contact" className="header__Btn">
+      <Link to="/contact" className={linkClass}>
         CONTACT
       </Link>
     </>
--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -24,6 +24,20 @@ const HeaderMobile = ({ headerStyle }) => {
     </svg>
   );
 
+  const cross = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="currentColor"
+      class="bi bi-x-lg"
+      viewBox="0 0 16 16"
+    >
+      <path d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
+      <path d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" />
+    </svg>
+  );
+
   return (
     <>
       <nav className={headerClass}>
@@ -36,7 +50,10 @@ const HeaderMobile = ({ headerStyle }) => {
               menu ? "header__MobLinksDisplay" : "header__MobLinksHide"
             }
           >
-            <HeaderLinks />
+            <HeaderLinks mobile={true} />
+            <button className="header__MobBtn" onClick={toggleMenu}>
+              {cross}
+            </button>
           </div>
         </div>
         <div>
